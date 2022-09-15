cc0d9a042e8c8996aa3f69af6899698f2e3564d5
= <=>
2022-09-15 13:55:55 -0300

    sa

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let jogador = "X";
 
+let vitorias_X = 0
+let vitorias_O = 0
+
+
 let casa01 = document.getElementById("casa01");
 let casa02 = document.getElementById("casa02");
 let casa03 = document.getElementById("casa03");
@@ -9,6 +13,9 @@ let casa06 = document.getElementById("casa06");
 let casa07 = document.getElementById("casa07");
 let casa08 = document.getElementById("casa08");
 let casa09 = document.getElementById("casa09");
+
+let placar = document.getElementById("placar");
+
 
 function jogada(casa) {
   if (casa.innerHTML === "") {
@@ -32,35 +39,69 @@ function verifica_ganhador() {
     casa01.innerHTML === casa03.innerHTML
   ) {
     alert(`Temos uma ganhador: ${casa01.innerHTML}`);
+    vitorias(casa01.innerHTML);
   } else if (
     casa04.innerHTML != "" &&
     casa04.innerHTML === casa05.innerHTML &&
     casa04.innerHTML === casa06.innerHTML
   ) {
     alert(`Temos uma ganhador: ${casa04.innerHTML}`);
+    vitorias(casa04.innerHTML);
   } else if (
     casa07.innerHTML != "" &&
     casa07.innerHTML === casa08.innerHTML &&
     casa07.innerHTML === casa09.innerHTML
   ) {
     alert(`Temos uma ganhador: ${casa07.innerHTML}`);
+    vitorias(casa07.innerHTML);
   } else if (
     casa01.innerHTML != "" &&
     casa01.innerHTML === casa05.innerHTML &&
     casa01.innerHTML === casa09.innerHTML
   ) {
     alert(`Temos uma ganhador: ${casa01.innerHTML}`);
+    vitorias(casa01.innerHTML);
   } else if (
     casa02.innerHTML != "" &&
     casa02.innerHTML === casa05.innerHTML &&
     casa02.innerHTML === casa08.innerHTML
   ) {
     alert(`Temos uma ganhador: ${casa02.innerHTML}`);
+    vitorias(casa02.innerHTML);
   } else if (
     casa03.innerHTML != "" &&
     casa03.innerHTML === casa05.innerHTML &&
     casa03.innerHTML === casa07.innerHTML
   ) {
     alert(`Temos uma ganhador: ${casa03.innerHTML}`);
+    vitorias(casa03.innerHTML);
   }
 }
+function vitorias(vencedor) {
+
+  if (vencedor === "X")
+    vitorias_X = vitorias_X + 1;
+
+  else
+    vitorias_O = vitorias_O + 1;
+
+  placar.innerHTML = ` <p>placar</p>  <p> X:${vitorias_X}</p>  <P>O:${vitorias_O}</P> `;
+
+
+  function iniciarJogo(){
+
+  letcasas = document.getElementsByClassName("casa")
+  casas[0].innerHTML = "";
+  casas[1].innerHTML = "";
+  casas[2].innerHTML = "";
+  casas[3].innerHTML = "";
+  casas[4].innerHTML = "";
+  casas[5].innerHTML = "";
+  casas[6].innerHTML = "";
+  casas[7].innerHTML = "";
+  casas[8].innerHTML = "";
+  casas[9].innerHTML = "";
+} }
+
+let quem_inicia = prompt("informe x ou o").toLocaleUpperCase();
+jogador = quem_inicia;
